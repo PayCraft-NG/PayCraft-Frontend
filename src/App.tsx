d776e9e77280bef07f4f928c96443e33f02f98d6
jason aghedo <jason.aghedo@gmail.com>
2024-09-20 14:24:02 +0100

    feat: setup dashboard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,50 @@
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import SignUpPage from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import MainLayout from "./components/dashboard/MainLayout";
 
 // Desc: Main App component for the application.
 function App() {
 	const location = useLocation();
 	return (
 		<Routes>
-			<Route path="/">
-				<Route
-					path="/"
-					element={
-						<Navigate
-							to="/login"
-							state={{ from: location }}
-							replace
-						/>
-					}
-				/>
+			<Route
+				path="/"
+				element={
+					<Navigate
+						to="/dashboard"
+						state={{ from: location }}
+						replace
+					/>
+				}
+			/>
 
-				<Route
-					path="login"
-					element={<LoginPage />}
-				/>
+			<Route
+				path="login"
+				element={<LoginPage />}
+			/>
 
-				<Route
-					path="signup"
-					element={<SignUpPage />}
-				/>
+			<Route
+				path="signup"
+				element={<SignUpPage />}
+			/>
 
-				{/* 404 */}
+			<Route
+				path="dashboard"
+				element={<MainLayout />}
+			>
 				<Route
-					path="*"
-					element={<>404</>}
+					path=""
+					element={<Dashboard />}
 				/>
 			</Route>
+
+			{/* 404 */}
+			<Route
+				path="*"
+				element={<>404</>}
+			/>
 		</Routes>
 	);
 }
